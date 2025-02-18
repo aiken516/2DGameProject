2873849c82138b551e6ef2000bb250cf9aa520bb
aiken516 <51198185+aiken516@users.noreply.github.com>
2025-02-18 11:44:55 +0900

    feat : UI 기능 추가

---
+++ UI.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DGameProject
+{
+    public class UI : GameObject
+    {
+        public UI()
+        { 
+        }
+
+        public UI(int x, int y, string text)
+        {
+            X = x;
+            Y = y;
+            Text = text;
+            isOn = false;
+        }
+
+        ~UI() 
+        { 
+        }
+
+        protected string Text;
+        protected bool isOn = false;
+
+        public void SetText(string text)
+        {
+            Text = text;
+            isOn = true;
+        }
+
+        public override void Render()
+        {
+            if (isOn)
+            {
+                Console.SetCursorPosition(X, Y);
+                Console.Write(Text);
+            }
+        }
+    }
+}
